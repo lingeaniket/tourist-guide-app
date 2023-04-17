a2c6d456b201a8df6b6f0677931d1cd481930762
lingeaniket <linge.aniket.10@gmail.com>
2023-04-17 17:39:40 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
 //*******Destination Component**********
 
-import React from 'react'
-import Destination from './Components/Destination/Destination';
-import './Components/Destination/Destination.css'
-
-function App() {
-  return (
-    <div>
-      <Destination />
-    </div>
-    
-  );
-}
-
-export default App;
-
-
-//*********Feedback Component***********
-
-// import { useState } from 'react';
 // import React from 'react'
-// import './Components/Feedback/Feedback.css'
-// import Feedback from './Components/Feedback/Feedback';
+// import Destination from './Components/Destination/Destination';
+// import './Components/Destination/Destination.css'
 
 // function App() {
-//   const [openReview, setOpenReview] = useState(false);
-
-//   const handleReview=()=>{
-//     if(openReview){
-//       setOpenReview(false);
-//     } else {
-//       setOpenReview(true);
-//     }
-//   }
 //   return (
 //     <div>
-//       <button onClick={handleReview}>Open review</button>
-//       {openReview && (<Feedback handleClick={handleReview}/>)}
+//       <Destination />
 //     </div>
     
 //   );
 // }
 
 // export default App;
+
+
+//*********Feedback Component***********
+
+import { useState } from 'react';
+import React from 'react'
+import './Components/Feedback/Feedback.css'
+import Feedback from './Components/Feedback/Feedback';
+
+function App() {
+  const [openReview, setOpenReview] = useState(false);
+
+  const handleReview=()=>{
+    if(openReview){
+      setOpenReview(false);
+    } else {
+      setOpenReview(true);
+    }
+  }
+  return (
+    <div>
+      <button onClick={handleReview}>Open review</button>
+      {openReview && (<Feedback handleClick={handleReview}/>)}
+    </div>
+    
+  );
+}
+
+export default App;
